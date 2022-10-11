fbe89dfa28ce579744e24843909dda52314642d4
Radon Rosborough <radon@intuitiveexplanations.com>
2022-10-11 16:51:56 -0700

    Initial thing

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from urllib.parse import urlparse
+
+import redis
+
+url = urlparse(os.environ.get("REDIS_URL"))
+r = redis.Redis(
+    host=url.hostname,  # type: ignore
+    port=url.port,  # type: ignore
+    username=url.username,  # type: ignore
+    password=url.password,  # type: ignore
+    ssl=True,
+    ssl_cert_reqs=None,
+)
